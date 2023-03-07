2a81614c8f00f9d08430541eed0e938d79854316
OUAG <OUAG@fsw.com.mx>
2023-03-07 11:58:05 -0600

    mail contact

--- mailContact.php
+++ mailContact.php
@@ -5,11 +5,13 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+
 $mailheader = "From:".$name. "<" .$email. "> \r\n";
 
 $emailReceiver = "oulises5552@gmail.com";
 
 mail($emailReceiver, $subject, $message, $mailheader) or die("Error!");
+
 
 echo "Mensaje enviado exitosamente";
 
